a76d51a9eaf2a5ed9dfb94f5dea961c9d6dc1df6
Dharmendra Verma <sdharmendra8858@gmail.com>
2024-01-27 16:34:32 +0530

    implemented the sqs with localstack

---
+++ src/app.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get("/list")
+  getList() {
+    return this.appService.getQueueList();
+  }
+
+  @Post("/create")
+  createQueue(@Body() body: {queueName: string, fifoQueue?: boolean}) {
+    return this.appService.createQueue(body)
+  }
+
+  @Post("/delete")
+  deleteQueue(@Query('queueUrl') queueUrl: string ){
+    return this.appService.deleteQueue(queueUrl)
+  }
+
+  @Post("/sendMessage")
+  publishMessage(@Body() body: {message: string, attributes?:Record<string, any>}, @Query() queueData: {queueUrl: string, msgGroupId: string}){
+    return this.appService.publishMessage(queueData.queueUrl, queueData.msgGroupId, body)
+  }
+
+  @Post("/getMessage")
+  getMessage(@Query("queueUrl") queueUrl: string){
+    return this.appService.receiveMessage(queueUrl);
+  }
+
+  @Post("/deleteMessage")
+  deleteMessage(@Query("queueUrl") queueUrl: string, @Body("receiptHandler") receiptHandler: string){
+    return this.appService.deleteMessage(queueUrl, receiptHandler)
+  }
+}
